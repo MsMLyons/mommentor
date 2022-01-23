f40cf2f6fc759214b30e05d674c8e79151ed580c
MsMLyons <ms.markilyons@gmail.com>
2022-01-23 16:39:59 -0500

    File update & begin server

---
+++ client/src/components/User.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const User = () => {
+    return (
+        <div>
+            <h1>This is your profile page</h1>
+        </div>
+    )
+}
+
+export default User
---
+++ client/src/views/HomeView.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HomeView = () => {
+    return (
+        <div>
+            <header>
+                Login | Register
+            </header>
+            <h1>Mom Mentor</h1>
+            <hr />
+            <h2>Welcome to Mom Mentor</h2>
+            <h3>Hi, Moms! Hello Mentors!</h3>
+            <p>Mom mentor is a social network that aims to connect moms who have had to leave the workforce or who are interested in a career change with other women in a position to offer mentorship and support in this endeavor.</p>
+        </div>
+    )
+}
+
+export default HomeView;
